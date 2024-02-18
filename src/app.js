41eb674a8bb8f9ad0e6afe63e10611f4d9e546c7
LRN251 <150847992+LRN251@users.noreply.github.com>
2024-02-18 17:11:06 +0000

    First commit Random Card Generator Project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+const symbols = ["heart", "diamond", "spade", "club"];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let symbolUp = document.querySelector(".symbolUp");
+let number = document.querySelector(".number");
+let symbolDown = document.querySelector(".symbolDown");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function selectCard(arr1, arr2) {
+  let cardArr = [];
+  cardArr.push(arr1[Math.floor(Math.random() * arr1.length)]);
+  cardArr.push(arr2[Math.floor(Math.random() * arr2.length)]);
+  return cardArr;
+}
 
+function generateRandomCard() {
+  let card = selectCard(symbols, values);
+  symbolUp.classList.add(card[0]);
+  symbolDown.classList.add(card[0]);
+  number.innerHTML = card[1];
+  console.log(card);
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
 };
